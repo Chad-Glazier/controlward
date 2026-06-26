7f125893912983e89b10af4df4e198dfb52d6518
Chad Glazier <chadglazier@outlook.com>
2026-06-25 20:44:31 -0700

    tweaks to footer

--- client/src/pages/layout/Footer.tsx
+++ client/src/pages/layout/Footer.tsx
@@ -1,13 +1,33 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./Footer.module.css";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
-    const year = new Date().getFullYear();
-
     return (
         <footer className={styles.footer}>
             <div className={styles.content}>
-                <small>&#xA9; {year} Chad Glazier.</small>
-                <small>Not affiliated with Riot Games</small>
+                <ul>
+                    <li>
+                        <FontAwesomeIcon icon={faGithub} />
+                        &nbsp;
+                        <a 
+                            target="_blank"
+                            href="https://github.com/Chad-Glazier/controlward"
+                        >
+                            Source
+                        </a>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        Control Ward is not endorsed by Riot Games and does not 
+                        reflect the views or opinions of Riot Games or anyone 
+                        officially involved in producing or managing League of 
+                        Legends. League of Legends and Riot Games are trademarks 
+                        or registered trademarks of Riot Games, Inc. League of 
+                        Legends &copy; Riot Games, Inc.
+                    </li>
+                </ul>
                 <div className={styles.contact}>
                     <small>
                         <span>Contact:</span>&nbsp;
